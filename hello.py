04ac7fa7b8cfeb2e28be85fa3a50a5d8fc8fbe78
Sken <k18056kk@aitech.ac.jp>
2020-12-18 22:59:29 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    name = "Hello World"
+    # 変更
+    return render_template('test.html', title='flask test', name=name)
+
+
+@app.route("/good")
+def good():
+    name = "good"
+    return render_template('test2.html', title='flask test', name=name)
+
+
+# おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
